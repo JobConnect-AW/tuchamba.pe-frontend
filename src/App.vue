b13e542cb7f5d9625c427268085fc14a935a9bc0
asmip10 <longsamata47@gmail.com>
2025-05-13 18:30:20 -0500

    build: Enviroment state

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <h1>Hola</h1>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,100 @@
+// import { createApp } from 'vue'
+// import { createPinia } from 'pinia'
+//
+// import App from './App.vue'
+// import router from './router'
+//
+// const app = createApp(App)
+//
+// app.use(createPinia())
+// app.use(router)
+//
+// app.mount('#app')
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from './app.vue'
 
-import App from './App.vue'
-import router from './router'
+// Vue Router
+import router from "./router/index.js";
 
-const app = createApp(App)
+// PrimeVue
+import PrimeVue from 'primevue/config';
 
-app.use(createPinia())
-app.use(router)
+// PrimeVue CSS
+import 'primeflex/primeflex.css';
 
-app.mount('#app')
+// PrimeVue Theme
+import Aura from '@primevue/themes/aura';
+
+// PrimeVue Icons
+import 'primeicons/primeicons.css';  // Asegúrate de importar PrimeIcons
+
+// Componentes de PrimeVue
+import Toast from "primevue/toast";
+import Toolbar from "primevue/toolbar";
+import Card from "primevue/card";
+import Column from "primevue/column";
+import Button from "primevue/button";
+import ConfirmDialog from "primevue/confirmdialog";
+import Checkbox from "primevue/checkbox";
+import DataTable from "primevue/datatable";
+import Dialog from "primevue/dialog";
+import FileUpload from "primevue/fileupload";
+import FloatLabel from "primevue/floatlabel";
+import IconField from "primevue/iconfield";
+import InputIcon from "primevue/inputicon";
+import InputText from "primevue/inputtext";
+import InputNumber from "primevue/inputnumber";
+import Menu from "primevue/menu";
+import Rating from "primevue/rating";
+import Row from "primevue/row";
+import Tag from "primevue/tag";
+import Textarea from "primevue/textarea";
+import Drawer from "primevue/drawer";
+import Select from "primevue/select";
+import ConfirmationService from "primevue/confirmationservice";
+import DialogService from "primevue/dialogservice";
+import ToastService from "primevue/toastservice";
+import Splitter from "primevue/splitter";
+
+import SelectButton from "primevue/selectbutton";
+
+import { createPinia } from "pinia";
+
+const app = createApp(App);
+
+
+app.use(router);
+
+const pinia = createPinia();
+app.use(pinia);
+
+app.use(PrimeVue, { theme: { preset: Aura }, ripple: true })
+  .use(ConfirmationService)
+  .use(DialogService)
+  .use(ToastService)
+  .component('pv-button', Button)
+  .component('pv-card', Card)
+  .component('pv-column', Column)
+  .component('pv-confirm-dialog', ConfirmDialog)
+  .component('pv-checkbox', Checkbox)
+  .component('pv-data-table', DataTable)
+  .component('pv-dialog', Dialog)
+  .component('pv-select', Select)
+  .component('pv-select-button', SelectButton)
+  .component('pv-file-upload', FileUpload)
+  .component('pv-float-label', FloatLabel)
+  .component('pv-icon-field', IconField)
+  .component('pv-input-icon', InputIcon)
+  .component('pv-input-text', InputText)
+  .component('pv-input-number', InputNumber)
+  .component('pv-menu', Menu)
+  .component('pv-rating', Rating)
+  .component('pv-row', Row)
+  .component('pv-drawer', Drawer)
+  .component('pv-tag', Tag)
+  .component('pv-textarea', Textarea)
+  .component('pv-toolbar', Toolbar)
+  .component('pv-toast', Toast)
+  .component('pv-splitter', Splitter);
+
+app.mount('#app');
