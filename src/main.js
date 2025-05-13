bd4acc36c78e6b2e3b316df2cc862ff2d58c37c8
flendoh <flenditomlgcreations@gmail.com>
2025-05-12 20:12:46 -0500

    Revert "feat: add dashboard, offers, and db.json"
    This reverts commit 2219421dbf72527996b256303fdb52a74a4e7c09.

--- src/main.js
+++ src/main.js
@@ -1,32 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import Aura from '@primeuix/themes/aura';
+
 import App from './App.vue'
-import PrimeVue from 'primevue/config';
 import router from './router'
 
-import Dropdown from 'primevue/dropdown';
-import Checkbox from 'primevue/checkbox';
-import Button from 'primevue/button';
-import Panel from 'primevue/panel';
-import Chart from 'primevue/chart';
+const app = createApp(App)
 
-
-const app = createApp(App);
-app.component('Panel', Panel);
-app.component('Chart', Chart);
-app.component('Dropdown', Dropdown);
-app.component('Checkbox', Checkbox);
-app.component('Button', Button);
 app.use(createPinia())
-app.use(PrimeVue, {
-
-});
 app.use(router)
 
 app.mount('#app')
-
-
-
-
-
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WorkerDashboard from '@/app/dashboard/pages/WorkerDashboard.vue'
-import CustomerDashboard from '@/app/dashboard/pages/CustomerDashboard.vue'
-import SearchOffers from '@/app/offers/pages/SearchOffers.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/dashboard',
-      children: [
-        { path: 'worker', component: WorkerDashboard },
-        { path: 'customer', component: CustomerDashboard }
-      ]
-    },
-    {
-      path: '/search-offers',
-      component: SearchOffers
-    }
-  ],
+  routes: [],
 })
 
 export default router
