2219421dbf72527996b256303fdb52a74a4e7c09
flendoh <flenditomlgcreations@gmail.com>
2025-05-12 20:09:15 -0500

    feat: add dashboard, offers, and db.json

---
+++ src/app/dashboard/components/PieChart.vue
@@ -0,0 +1,44 @@
+
+<template>
+    <Chart type="pie" :data="chartData" :options="chartOptions"/>
+</template>
+
+<script setup>
+    const props = defineProps({
+        value_1: {
+            type: Object,
+            default: () => ({
+                label: '',
+                value: 0
+            })
+        },
+        value_2: {
+            type: Object,
+            default: () => ({
+                label: '',
+                value: 0
+            })
+        }
+    });
+    const {value_1, value_2} = props;
+    const chartData = {
+        labels: [value_1.label, value_2.label],
+        datasets: [{
+            data: [value_1.value, value_2.value],
+            backgroundColor: ['#2A58AE', '#FF9900'],
+            borderWidth: 0
+        }]
+    };
+    
+    const chartOptions = {
+        plugins: {
+            legend: {
+                display: true,
+                position: 'bottom',
+            },
+            tooltip: {
+                enabled: true
+            }
+        }
+    };
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import WorkerDashboard from '@/app/dashboard/pages/WorkerDashboard.vue'
+import CustomerDashboard from '@/app/dashboard/pages/CustomerDashboard.vue'
+import SearchOffers from '@/app/offers/pages/SearchOffers.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/dashboard',
+      children: [
+        { path: 'worker', component: WorkerDashboard },
+        { path: 'customer', component: CustomerDashboard }
+      ]
+    },
+    {
+      path: '/search-offers',
+      component: SearchOffers
+    }
+  ],
 })
 
 export default router
