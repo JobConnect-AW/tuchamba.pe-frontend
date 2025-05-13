55ec27a09489ceed2581768e3ac2a9f1cae3aa5d
flendoh <flenditomlgcreations@gmail.com>
2025-05-12 20:34:11 -0500

    feat(dashboard): add worker and customer dashboards with chart components

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import WorkerDashboard from '@/app/dashboard/views/WorkerDashboard.vue'
+import CustomerDashboard from '@/app/dashboard/views/CustomerDashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+      {
+        path: '/dashboard',
+        children: [
+          { path: 'worker', component: WorkerDashboard },
+          { path: 'customer', component: CustomerDashboard }
+        ]
+      }
+  ],
 })
 
 export default router
