92d6f17ea157c66824d2802e64db17140dffed38
flendoh <flenditomlgcreations@gmail.com>
2025-05-12 20:43:56 -0500

    feat(offers): add search offers feature with components and routing

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 import router from './router'
 
-const app = createApp(App)
+import Dropdown from 'primevue/dropdown';
+import Checkbox from 'primevue/checkbox';
+import Button from 'primevue/button';
 
+
+const app = createApp(App);
+app.component('Dropdown', Dropdown);
+app.component('Checkbox', Checkbox);
+app.component('Button', Button);
 app.use(createPinia())
+app.use(PrimeVue, {
+
+});
 app.use(router)
 
 app.mount('#app')
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import SearchOffers from '@/app/offers/views/SearchOffers.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{
+    path: '/search-offers',
+    component: SearchOffers
+  }],
 })
 
 export default router
