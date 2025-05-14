906b9bc9af556fd9c873a7ad9e8bf6de65eaaa0b
flendoh <flenditomlgcreations@gmail.com>
2025-05-14 15:12:16 -0500

    refactor(offers): move SearchOffers component from views to pages directory

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SearchOffers from '@/app/offers/views/SearchOffers.vue'
+import SearchOffers from '@/app/offers/pages/SearchOffers.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [{
